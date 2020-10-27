b3cec5b47c7996d983b5539c30bd9990b63af282
unsivil <unsivilaudio@gmail.com>
2020-10-27 13:28:20 -0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const app = props => {
+    return (
+        <div className='Container'>
+            <h1>My New App Component</h1>
+        </div>
+    );
+};
+
+export default app;
